ce8252e2db412c39d2f0751858f1fd4d3fd19023
1984Johnny1984 <jonathanoliveira.joq46@gmail.com>
2025-07-30 23:34:26 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Signals />
+  </div>
+</template>
+
+<script>
+import Signals from './components/Signals.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Signals
+  }
+}
+</script>
