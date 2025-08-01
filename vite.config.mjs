b4cb52e0f5c18884da691b7276f3aef8ee390cbc
Jonathan Oliveira <jonathanoliveira.joq46@gmail.com>
2025-08-01 20:25:24 -0300

    corrigido package.json com scripts build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'dist'
-  }
-})
+export default {
+  plugins: [vue()]
+}
