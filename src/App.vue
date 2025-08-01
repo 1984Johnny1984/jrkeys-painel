f50ad569be78306b628fa07f7e02ebe9d3431745
1984Johnny1984 <jonathanoliveira.joq46@gmail.com>
2025-08-01 13:20:36 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    <h1>Painel JR KEYS - Sinais em Tempo Real</h1>
+    <SignalCard />
+  </div>
+</template>
+
+<script setup>
+import SignalCard from './components/SignalCard.vue'
+</script>
+
+<style src="./style.css"></style>
