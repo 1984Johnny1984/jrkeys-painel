d41e616a363e7d759f34ff00c4549e40139fc071
1984Johnny1984 <jonathanoliveira.joq46@gmail.com>
2025-08-01 09:23:47 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://loydenntqrwqrzdinwkx.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNzAwODQwNjYzLCJleHAiOjIwMTYxMjY2NjN9.iKZhEemxf2e3IhP80KPX7oZCNnReyk9rszgykiqvkeU'
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
